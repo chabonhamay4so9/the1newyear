51a39076d365f0f234074cb02a4b1278efdd96f0
chabonhamay4so9 <93881933+chabonhamay4so9@users.noreply.github.com>
2023-12-02 05:00:14 +0700

    Add files via upload

---
+++ next.config.js
@@ -0,0 +1,9 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  env: {
+    url:"https://bookpostponemoreover.com/k439c0hqe8?key=19fc64e0f360f39f6a49bb38c40675d1"
+  },
+}
+
+module.exports = nextConfig
